0b354fb36b5d1c1cbdc630195b06077c1f8ca884
Breyana Scales <breyana@Breyanas-MacBook-Pro.local>
2017-01-05 09:34:56 -0800

    Sum Odd Fibonacci fixed

--- algorithms/SumOddFibonacci.js
+++ algorithms/SumOddFibonacci.js
@@ -5,30 +5,31 @@
 // For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than 10 are 1, 1, 3, and 5.
 
 
-var reducer = function(a, b) {
-  return a + b;
+var reducer = function(a, b){
+    return a + b;
 };
 
-function isPrime(value) {
-  for(var i = 2; i < value; i++) {
-      if(value % i === 0) {
-          return false;
-      }
-  }
-  return value > 1;
-}
+var compiler = function(num){
+  var i;
+  var fib = []; // Initialize array!
+  fib[0] = 0;
+  fib[1] = 1;
+    for(i=2; (fib[i-2] + fib[i-1]) <= num; i++) {
+      fib[i] = fib[i-2] + fib[i-1];
+    }
+    return fib;
+  };
 
-function sumPrimes(value) {
-  var primesArray = [];
-  var fullArray = [];
-  for (var i = 0; i <= value; i++) {
-    fullArray.push(i);
-  }
-  for (var i = 0; i <= fullArray.length; i++) {
-    if (isPrime(fullArray[i]) === true) {
-      primesArray.push(fullArray[i]);
+
+function sumFibs(num){
+  var fibArray = compiler(num);
+  var oddFibs = [];
+  for(var odds = 0; odds <= fibArray.length-1; odds++){
+    if(fibArray[odds] % 2 !== 0){
+      oddFibs.push(fibArray[odds]);
     }
   }
-
-  return primesArray.reduce(reducer, 0);
+  return oddFibs.reduce(reducer,0);
 }
+
+sumFibs(10);
